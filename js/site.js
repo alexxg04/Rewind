c938a5d3445f043f00b289fea7ac27c1f3ab16d4
Alejandro Garcia <alex-04@live.com>
2022-07-22 10:00:36 -0700

    Working app
    Application is working, missing notes on code part.

--- js/site.js
+++ js/site.js
@@ -1,6 +1,39 @@
 
-//Call Hello World
-function HelloWorld(){
-    alert("Hello World");
-    console.log("Hello World");
-};
+//Get the strings from the page
+
+
+function getValue(){
+
+    document.getElementById("alert").classList.add("invisible");
+    let userString  = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    
+    displayString(revString);
+}
+
+
+
+//Reverse the string
+function reverseString(userString){
+
+    let revString = [];
+    //reverse the string with a for loop
+    
+    for (let index = userString.length -1; index >= 0; index--) {
+        revString += userString[index];
+        
+    }
+    return revString;
+}
+
+
+
+///Display the reverse string
+function displayString(revString){
+
+    //Write to the page
+    //show the alert box
+    document.getElementById("msg").innerHTML = `Your String reversed is: ${revString}`;
+    //Show athe alert box
+    document.getElementById("alert").classList.remove("invisible");
+}
